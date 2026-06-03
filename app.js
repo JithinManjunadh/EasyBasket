7945216859ecef48100266fd0f92a28036cca257
JithinManjunadh <jithinmanjunadh2003@gmail.com>
2026-06-03 16:52:14 +0530

    port fixed

--- app.js
+++ app.js
@@ -103,6 +103,8 @@ app.use((err,req,res,next)=>{
     let {statusCode=500,message="Something went wrong!"}=err;
     res.status(statusCode).render("error",{message});
 });
-app.listen(3000, () => {
-    console.log('Lumeo running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
